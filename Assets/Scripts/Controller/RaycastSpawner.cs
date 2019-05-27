0e09c3b9621a75b6930ee8907a5b6a4e7987ba1b
Roman Knyazev <roman.knyazev@omega-r.com>
2019-05-27 13:35:01 +0300

    Init

---
+++ Assets/Scripts/Controller/RaycastSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Assets
+{
+    public sealed class RaycastSpawner : MonoBehaviour
+    { 
+        [SerializeField] private int _gridCellLayer = 5;
+        [SerializeField] private GameObject _prefab;
+        private void Update()
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (!Physics.Raycast(ray, out RaycastHit hit, 100.0f)) return;
+                if (hit.transform.gameObject.layer == _gridCellLayer)//layer 11(TurretLayer)
+                {
+                    Instantiate(_prefab, hit.transform.position, Quaternion.identity);
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets{
+public sealed class GridManager : MonoBehaviour
+{
+    [SerializeField] private Transform gridPrefab;
+    [SerializeField] private int rows = 2;
+    [SerializeField] private int columns = 2;
+    private void Start()
+    {
+        for (int r = -rows; r < rows; r++)
+        {
+            for (int c = -columns; c < columns; c++)
+            {
+                var celltransform = new Vector3(r + 1.5f, 0, c + 0.5f);
+                Instantiate(gridPrefab, celltransform, Quaternion.identity);
+            }
+        }
+    }
+}
+}
